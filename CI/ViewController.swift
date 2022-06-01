b07352d25f03261d35dd1231661ff316fe3807cd
vudinhdjv <vu.dinh@drjoy.vn>
2022-06-01 17:21:23 +0700

    first commit

---
+++ CI/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ContinueDelivery
+//
+//  Created by Dinh Van Vu on 31/03/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        let  a = 122341221;
+    }
+
+
+}
+
